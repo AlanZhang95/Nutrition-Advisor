7dbd073410145592914c18726ed27d75d2034e13
lmiao4 <lmiao4@illinois.edu>
2019-04-18 21:48:40 -0500

    slightly improve Food list view

--- NutritionAdvisor/frontend/src/components/Foods.js
+++ NutritionAdvisor/frontend/src/components/Foods.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { List, Card } from 'antd';
 
 const source_dict = {
-                  "pt": "Main Protein Source",
-                  "ft": "Main Fat Source",
-                  "ct": "Main Carbonhydrate Source",}
+    "pt": "Main Protein Source",
+    "ft": "Main Fat Source",
+    "ct": "Main Carb Source",
+  }
 
 const Foods = (props) => {
     return (
       <List
         grid={{
-          gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3,
+          gutter: 15, xs: 1, sm: 2, md: 3, lg: 3, xl: 5, xxl: 5,
         }}
         dataSource={props.data}
         renderItem={item => (
           <List.Item>
-            <Card title={<a href={`foods/${item.id}`}>{item.name}</a>}>
+            <Card title={<a href={`foods/${item.id}`}>{item.name}</a>}> 
                 {source_dict[item.source_type]}
             </Card>
           </List.Item>
