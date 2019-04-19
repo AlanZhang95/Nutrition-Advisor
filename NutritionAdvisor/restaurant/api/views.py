41660b280da104cb37b6e18fc956bfdba7b87a07
lmiao4 <lmiao4@illinois.edu>
2019-04-18 21:07:50 -0500

    add individual restaurant viewsets on each goal

--- NutritionAdvisor/restaurant/api/views.py
+++ NutritionAdvisor/restaurant/api/views.py
@@ -6,3 +6,16 @@ from rest_framework.response import Response
 class RRViewSet(viewsets.ModelViewSet):
     serializer_class = RRSerializer
     queryset = Recommendation.objects.all()
+
+
+class LoseViewSet(viewsets.ModelViewSet):
+    serializer_class = RRSerializer
+    queryset = Recommendation.objects.values('name', 'goal', 'choices', 'aggregate',).filter(goal="Lose Fat")
+
+class TransViewSet(viewsets.ModelViewSet):
+    serializer_class = RRSerializer
+    queryset = Recommendation.objects.values('name', 'goal', 'choices', 'aggregate',).filter(goal="Transform")
+
+class GainViewSet(viewsets.ModelViewSet):
+    serializer_class = RRSerializer
+    queryset = Recommendation.objects.values('name', 'goal', 'choices', 'aggregate',).filter(goal="Gain Muscle")
